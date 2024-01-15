69fb2f53946215a046a262815f43ab0e5eba26c3
bertrand <bertranddilly@yahoo.fr>
2024-01-15 15:38:14 +0100

    add config netlify deploy

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,16 @@
 module.exports = ({ env }) => ({
   "netlify-deployments": {
     enabled: true,
+    config: {
+      accessToken: process.env.NETLIFY_DEPLOYMENTS_PLUGIN_ACCESS_TOKEN,
+      sites: [
+        {
+          name: 'darling-sprinkles-62ef0b',
+          id: "824173c6-8e22-48b6-935e-af35607fabd0",
+          buildHook: "https://api.netlify.com/build_hooks/65a542f80914eb16f79eb17c",
+          branch: 'master' // optional
+        }
+      ]
+    },
   },
 });
